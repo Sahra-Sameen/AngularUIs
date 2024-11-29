04443790e07bac60f85d7cd42db7eb1a729ecc69
SAHRA M.S.F <126109790+Sahra-Sameen@users.noreply.github.com>
2024-11-29 10:16:47 +0530

    POS UI component created

---
+++ src/app/Components/pos-ui/pos-ui.component.ts
@@ -0,0 +1,12 @@
+import { Component } from '@angular/core';
+
+@Component({
+  selector: 'app-pos-ui',
+  standalone: true,
+  imports: [],
+  templateUrl: './pos-ui.component.html',
+  styleUrl: './pos-ui.component.scss'
+})
+export class POSUIComponent {
+
+}
--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,12 +2,13 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { OrdersUIComponent } from './Components/orders-ui/orders-ui.component';
 import { DishListingUiComponent } from './Components/dish-listing-ui/dish-listing-ui.component';
+import { POSUIComponent } from './Components/pos-ui/pos-ui.component';
 
 
 @Component({
   selector: 'app-root',
   standalone: true,
-  imports: [RouterOutlet, OrdersUIComponent, DishListingUiComponent],
+  imports: [RouterOutlet, OrdersUIComponent, DishListingUiComponent, POSUIComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
