845e091d6356367e70b7e5f2ace22dc807e8f9d0
SAHRA M.S.F <126109790+Sahra-Sameen@users.noreply.github.com>
2024-12-02 18:21:34 +0530

    sidebar menu component generated

--- src/app/Components/pos-ui/pos-ui.component.ts
+++ src/app/Components/pos-ui/pos-ui.component.ts
@@ -3,11 +3,12 @@ import { NavbarMenuComponent } from './navbar-menu/navbar-menu.component';
 import { OrderListComponent } from './order-list/order-list.component';
 import { CategoryListComponent } from './category-list/category-list.component';
 import { MenuListComponent } from './menu-list/menu-list.component';
+import { SidebarMenuComponent } from './sidebar-menu/sidebar-menu.component';
 
 @Component({
   selector: 'app-pos-ui',
   standalone: true,
-  imports: [NavbarMenuComponent, OrderListComponent,CategoryListComponent, MenuListComponent],
+  imports: [NavbarMenuComponent, OrderListComponent,CategoryListComponent, MenuListComponent, SidebarMenuComponent],
   templateUrl: './pos-ui.component.html',
   styleUrl: './pos-ui.component.scss'
 })
